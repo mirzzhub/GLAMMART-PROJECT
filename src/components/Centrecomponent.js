95b43b5d0f63ad4c9590e4e65a4a5c5fb9afd139
mirzzhub <124986914+mirzzhub@users.noreply.github.com>
2023-11-21 12:29:44 +0530

    Add files via upload

---
+++ src/components/Centrecomponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ImageButtonDemo from './ImageButtonDemo'
+const Centrecomponent = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px', padding: '20px', maxWidth: 'px', margin: '0 auto' }}>
+      <ImageButtonDemo/>
+      <div style={{ margin: '20px 0' }}></div>
+    </div>
+        
+    
+  )
+  
+}
+
+export default Centrecomponent
---
+++ src/components/ImageButtonDemo.js
@@ -0,0 +1,123 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+
+const images = [
+  {
+    url: 'https://img.freepik.com/premium-photo/skin-care-products-healthcare-procedures-concept-hygiene-cosmetic-healthy-lifestyle-spa_266732-20971.jpg',
+    title: 'PERSONAL CARE',
+    width: '20%',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1483985988355-763728e1935b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzaGlvbnxlbnwwfHwwfHx8MA%3D%3D',
+    title: 'STYLES',
+    width: '20%',
+  },
+  {
+    url: 'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/themes/2153354507/settings_images/7e3ced6-1aae-8b4d-82f8-22eb1faccfce_cosmetic-bottles-flat-layed-blank-label-branding-mock-up-natural-beauty-product-concept-ai-generated.jpg',
+    title: 'NATURAL PRODUCTS',
+    width: '20%',
+  },
+];
+
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  height: 200,
+  [theme.breakpoints.down('sm')]: {
+    width: '100% !important', // Overrides inline-style
+    height: 100,
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiImageBackdrop-root': {
+      opacity: 0.15,
+    },
+    '& .MuiImageMarked-root': {
+      opacity: 0,
+    },
+    '& .MuiTypography-root': {
+      border: '4px solid currentColor',
+    },
+  },
+}));
+
+
+const ImageSrc = styled('span')({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center 40%',
+});
+
+const Image = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity'),
+}));
+
+const ImageMarked = styled('span')(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity'),
+}));
+
+export default function ButtonBaseDemo() {
+  return (
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
+      {images.map((image) => (
+        <ImageButton
+          focusRipple
+          key={image.title}
+          style={{
+            width: image.width,
+          }}
+        >
+          <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+          <ImageBackdrop className="MuiImageBackdrop-root" />
+          <Image>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              sx={{
+                position: 'relative',
+                p: 4,
+                pt: 2,
+                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+              }}
+            >
+              {image.title}
+              <ImageMarked className="MuiImageMarked-root" />
+            </Typography>
+          </Image>
+        </ImageButton>
+      ))}
+    </Box>
+  );
+}
